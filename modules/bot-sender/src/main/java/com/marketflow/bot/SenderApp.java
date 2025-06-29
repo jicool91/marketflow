dcedcf7a9441bd58f5ca648255d47ad904846eb9
jicool91 <jicool912@gmail.com>
2025-06-29 14:38:03 +0300

    Добавлены простые классы SenderApp и GeneratorApp

---
+++ modules/bot-sender/src/main/java/com/marketflow/bot/SenderApp.java
@@ -0,0 +1,15 @@
+package com.marketflow.bot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Простейшее приложение-отправитель.
+ */
+public class SenderApp {
+    private static final Logger log = LoggerFactory.getLogger(SenderApp.class);
+
+    public static void main(String[] args) {
+        log.info("Bot Sender started");
+    }
+}
